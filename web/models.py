18eb3813e1ca759d0b5bc9c51304b671b25cc6a9
mamrezz <itclow00@gmail.com>
2020-01-25 22:52:50 +0330

    freeze requirements and str to models

--- web/models.py
+++ web/models.py
@@ -8,9 +8,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.text, self.amount)
 
 class Income(models.Model):
     text = models.CharField(max_length=250)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.text, self.amount)
