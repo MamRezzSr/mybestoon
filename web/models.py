bf708e2d382ad51b045af8499b2f3b8a9ae1d95e
mamrezz <itclow00@gmail.com>
2020-01-28 22:22:14 +0330

    added submit expense and submit income api

--- web/models.py
+++ web/models.py
@@ -3,13 +3,19 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=250)
+    def __str__(self):
+        return "{} - {}".format(self.user, self.token)
+
 class Expense(models.Model):
     text = models.CharField(max_length=250)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
-        return "{}-{}".format(self.text, self.amount) 
+        return "{} - {}".format(self.text, self.amount) 
 
 class Income(models.Model):
     text = models.CharField(max_length=250)
@@ -17,4 +23,4 @@ class Income(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
-        return "{}-{}".format(self.text, self.amount)
+        return "{} - {}".format(self.text, self.amount)
--- web/views.py
+++ web/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from web.models import  User, Expense, Income, Token
+from datetime import datetime
 
 # Create your views here.
+
+@csrf_exempt
 def submit_expense(request):
-    return HttpResponse("Expense Submited")
+    this_token = request.POST["token"]
+    this_user = User.objects.filter(token__token=this_token).get()
+    now = datetime.now()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=now)
+    data = {
+        'status': 'ok',
+    }
+    return JsonResponse(data, encoder=JSONEncoder)
+    
+@csrf_exempt
+def submit_income(request):
+    this_token = request.POST["token"]
+    this_user = User.objects.filter(token__token=this_token).get()
+    now = datetime.now()
+    Income.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=now)
+    data = {
+        'status': 'ok',
+    }
+    return JsonResponse(data, encoder=JSONEncoder)
